833488a379ba479d353864e638ded29ae664021d
LesHank <henrik.berglund@gmail.com>
2022-04-25 23:16:12 +0200

    wind digits fix

--- src/components/Wind/Wind.js
+++ src/components/Wind/Wind.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import wind from "../../assets/wind.svg";
+import windy from "../../assets/windy.svg";
 
-const Wind = () => {
+const Wind = ({ wind, units }) => {
   return (
     <Container>
       <div className="container d-flex justify-content-around align-items-center mt-3">
-        <img src={wind} className="infoIconWind" />
+        <img src={windy} className="infoIconWind" />
         <h4>
           Wind <small>(gust wind)</small>
         </h4>
-        <h2>1 (2) m/s</h2>
+        <h2>
+          {Math.round(wind.speed)} ({Math.round(wind.gust)})
+        </h2>
       </div>
       <hr />
     </Container>
