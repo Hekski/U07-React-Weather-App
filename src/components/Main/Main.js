736f784d93bda3db6624c1cd84954528b3514026
LesHank <henrik.berglund@gmail.com>
2022-04-25 23:18:13 +0200

    UTC time conversion

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,47 +1,82 @@
-import React from "react";
 import "./Main.css";
 import { Container } from "react-bootstrap";
-import sunup from "../../assets/sunup.svg"
+import sunup from "../../assets/sunup.svg";
 import sundown from "../../assets/sundown.svg";
 import infoicon from "../../assets/infoicon.svg";
 
-const Main = () => {
-
+const Main = ({ main, weather, clouds, visibility, sys, timezone }) => {
   const unixTimeToDate = (UTC, timeZoneOffset) => {
     const dateTime = UTC * 1000 + timeZoneOffset;
     return new Date(dateTime);
   };
 
+  const options = {
+    time: {
+      hour: "2-digit",
+      minute: "2-digit",
+    },
+    date: {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+    },
+    sun: {
+      hour: "2-digit",
+      minute: "2-digit",
+    },
+  };
+
+  /* const dateTime = new Date(1650855933);
+  dateTime.toISOString();
+  console.log("HEJ" + dateTime); */
+
   return (
     <Container>
-      <div className="d-flex justify-content-around align-baseline">
-        <h1 className="temp">15 °</h1>
-        <div className="sunTime">
-          <div className="d-flex">
-            <img className="me-2" src={sunup} />
-            <p>06:32</p>
-          </div>
-          <div className="d-flex">
-            <img className="me-2" src={sundown} />
-            <p>20:32</p>
-          </div>
-        </div>
+      <h4>{weather[0].description}</h4>
+      <div className="d-flex justify-content-around align-items-center">
+        <h1 className="temp">
+          {Math.round(main.temp)}
+          <span>&deg;</span>
+        </h1>
+        <img src={infoicon} className="infoIcon" />
       </div>
-      <div>
-        <div className="d-flex flex-direction-row justify-content-around ms-5">
-          <div>
-            <h4 className="scale">°C | °F</h4>
-            <div className="info mt-5">
-              <h4>Feels like: 20°</h4>
-              <h4>Humidity: 40%</h4>
-              <h4>Rain: 100%</h4>
-              <h4>Cloudiness: 20%</h4>
-            </div>
-          </div>
-          <img src={infoicon} className="infoIcon" />
+
+      <div className="d-flex justify-content-around">
+        <div>
+          <img className="me-2" src={sunup} />
+          <h2>
+            {unixTimeToDate(sys.sunrise, timezone).toLocaleTimeString(
+              [],
+              options.sun
+            )}
+          </h2>
+        </div>
+        <div>
+          <img className="me-2" src={sundown} />
+          <h2>
+            {unixTimeToDate(sys.sunset, timezone).toLocaleTimeString(
+              [],
+              options.sun
+            )}
+          </h2>
         </div>
       </div>
 
+      <div>
+        <div className="d-flex mt-5 flex-direction-row justify-content-around align-items-center">
+          <div className="info">
+            <h4>Visibility: {visibility / 1000} km</h4>
+            <h4>
+              Feels like: {Math.round(main.feels_like)}
+              <span>&deg;</span>
+            </h4>
+          </div>
+          <div>
+            <h4>Humidity: {main.humidity} %</h4>
+            <h4>Cloudiness: {clouds} %</h4>
+          </div>
+        </div>
+      </div>
     </Container>
   );
 };
