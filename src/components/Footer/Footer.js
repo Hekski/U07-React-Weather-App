3d3229dcf45d07ea716234cea68174d63d54f93d
LesHank <henrik.berglund@gmail.com>
2022-04-30 19:14:48 +0200

    Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="pt-4">
+        <span>Henrik Berglund 2022, </span>
+        School Assignment @ Chas Academy, class FWD21
+      </p>
+      <p className="pb-5">
+        <a href="#">Weather App @ GitHub</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
